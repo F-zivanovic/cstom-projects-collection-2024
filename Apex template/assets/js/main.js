b1a2c941eedfff8bc481f74e5b817bd35077fec4
F-zivanovic <filip.zivanovic1999@gmail.com>
2022-07-20 17:31:30 +0200

    Add animations and Scroll to top btn

--- Apex template/assets/js/main.js
+++ Apex template/assets/js/main.js
@@ -14,3 +14,29 @@ for (listItem of listItems) {
     });
 }
 
+// Scroll to top
+let scrollBtn = document.querySelector('.scroll__top');
+scrollBtn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+    window.scroll({ top: 0, behavior: "smooth" });
+}
+
+
+window.onscroll = () => {
+    showScrollBtn();
+}
+
+function showScrollBtn() {
+
+    const sectionAnimation = document.querySelector('.about');
+    const sectionPosition = sectionAnimation.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3
+
+    const btnTop = document.querySelector('.scroll__top');
+
+    if (sectionPosition < screenPosition) {
+        btnTop.classList.add('animated');
+    }
+}
+
