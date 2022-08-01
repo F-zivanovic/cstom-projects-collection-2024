667432b2fb35f51479bd8fd199a17b684f2373c3
F-zivanovic <filip.zivanovic1999@gmail.com>
2022-08-01 12:09:29 +0200

    Add and style SERVICES section

--- Klean template/assets/js/main.js
+++ Klean template/assets/js/main.js
@@ -17,3 +17,51 @@ navMobileItem.forEach(function (item) {
         hamburger.classList.remove('open');
     });
 });
+
+// Services slider
+let rightArrow = document.querySelector('#right');
+let leftArrow = document.querySelector('#left');
+let pictures = document.querySelectorAll('.services-slider > img');
+
+let imgNumber = 0;
+
+function moveRight() {
+    hidePictures();
+    imgNumber++;
+
+    if (imgNumber === pictures.length) {
+        imgNumber = 0;
+    }
+
+    pictures[imgNumber].style.display = 'block';
+}
+
+function moveLeft() {
+    hidePictures();
+    imgNumber--;
+
+    if (imgNumber === -1) {
+        imgNumber = pictures.length - 1;
+    }
+
+    pictures[imgNumber].style.display = 'block';
+}
+
+
+rightArrow.addEventListener('click', moveRight);
+leftArrow.addEventListener('click', moveLeft);
+
+function hidePictures() {
+
+    // pictures.forEach(function (img) {
+    //     img.style.display = 'none';
+    // });
+
+    // for (img of pictures) {
+    //     img.style.display = 'none';
+    // }
+
+    pictures.forEach((img) => {
+        img.style.display = 'none';
+    });
+}
