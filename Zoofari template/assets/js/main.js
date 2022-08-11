c6623245a0aee9f3ac7e8b49caddaa926f15b0a2
F-zivanovic <filip.zivanovic1999@gmail.com>
2022-08-11 14:40:40 +0200

    Add and style NUMBERS section

--- Zoofari template/assets/js/main.js
+++ Zoofari template/assets/js/main.js
@@ -16,6 +16,47 @@ links.forEach((link) => {
     });
 });
 
+// Counter section
+let counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+document.addEventListener('scroll', scrollPage);
 
+function scrollPage() {
+    const scrollPos = window.scrollY;
 
+    if (scrollPos > 950 && !scrollStarted) {
+        countUp();
+        scrollStarted = true;
+    } else if (scrollPos < 700 && scrollStarted) {
+        reset();
+        scrollStarted = false;
+    }
+}
+
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+            const increment = target / 100;
+
+            if (c < target) {
+                counter.innerText = `${Math.ceil(c + increment)}`;
+                setTimeout(updateCounter, 75);
+            }
+            else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
+
+function reset() {
+    counters.forEach((counter) => (counter.innerHTML = '0'));
+}
 
