0aad3b2f24b4e4242f072a3ec2080ff48161d619
F-zivanovic <filip.zivanovic1999@gmail.com>
2022-11-10 17:08:08 +0100

    Add HTML structure, CSS styls and Swiper slider for Blog section

--- Fitness template/assets/js/script.js
+++ Fitness template/assets/js/script.js
@@ -47,3 +47,29 @@ var swiper = new Swiper(".reviews-slider", {
         },
     },
 });
+
+
+var swiper = new Swiper(".blog-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
